014ac656a200ba52c94bbe3c5fbf0180a8f5c981
angiebunk1 <103800228+angiebunk1@users.noreply.github.com>
2022-09-01 15:52:15 -0500

    still not working but adding code

--- client/src/js/index.js
+++ client/src/js/index.js
@@ -6,10 +6,13 @@ import 'bootstrap/dist/css/bootstrap.min.css';
   import Logo from '../images/logo.png';
 import Bear from '../images/bear.png';
 import Dog from '../images/dog.png';
-import { initdb } from './database';
+import { initdb, getDb, postDb } from './database';
+
 
 window.addEventListener('load', function () {
   initdb();
+  getDb();
+  postDb();
   document.getElementById('logo').src = Logo;
   document.getElementById('bearThumbnail').src = Bear;
   document.getElementById('dogThumbnail').src = Dog;
